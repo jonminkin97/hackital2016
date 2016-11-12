8cce25afe93f8e999f6d6d933985c7f230dd49a6
Jonathan <jonminkin@gmail.com>
2016-11-12 01:59:50 -0500

    new functions for getting temp and precipitation

--- isRaining.py
+++ isRaining.py
@@ -1,12 +1,27 @@
 import requests
 
-def apirequest(a, b):
+def apirequest(a, b): #a is lattitude, b is longitude, function creates api url to call
     api="http://api.openweathermap.org/data/2.5/weather?lat="
     api+=str(a)
     api+="&lon="
     api+=str(b)
     api+="&APPID=6cb2a52bb77a7c60fde88c2a1c7e4371"
     return api
+
+def precipitation():
+    weather = data['weather']
+    description = weather[0]['description']  # used to check weather description for rain
+    precipitation = ("rain" in description) | ("snow" in description) | ("sleet" in description) | (
+    "hail" in description) | ("showers" in description)
+    return precipitation
+
+def currentTemp():
+    temp = data['main']
+    currenttemp = temp['temp']  # finds current temperature in kelvin
+    currenttemp = (currenttemp - 273.15) * (9.0 / 5.0) + 32
+    return currenttemp
+
+
 
 lat=float(input("Enter Latitude"))
 long=float(input("Enter Longitude"))
@@ -16,10 +31,10 @@ apiurl=apirequest(lat,long)
 response=requests.get(apiurl)
 data=response.json()
 
-weather=data['weather']
-description=weather[0]['description']
-rainy="rain" in description
-print(rainy)
+print(precipitation(),currentTemp())
+#print("Current Temperature: ",currenttemp)
+#print("Precipitation: ",precipitation)
+
 
 
 
