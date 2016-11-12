84bef97267eaaebb6cdcb94e8252aa86fcd6a3d7
Pat Cody <patcody028@gmail.com>
2016-11-12 08:38:19 -0500

    Moved maps module to within Flask application

--- HackitalProject/HackitalProject/HackitalProject/python/maps.py
+++ HackitalProject/HackitalProject/HackitalProject/python/maps.py
@@ -13,14 +13,25 @@ class maps:
         geocode_result = self.gmaps.geocode(address)
         return geocode_result[0]['geometry']['location']
 
-        print("Address: ", address)
-        print("Latitude: ", geocode_result[0]['geometry']['location']['lat'])
-        print("Longitude: ",geocode_result[0]['geometry']['location']['lng'])
+    def distance_to_location(self):
+        origin = self.get_lat_long('419 7th St NW, Washington, DC')
+        destination = self.get_lat_long('1900 F St NW, Washington DC')
+        matrix = self.gmaps.distance_matrix(origin, destination)
+        print(matrix)
 
+
+
+"""
+TODO
+Walking distance to station
+Walking distance from loc A to loc B
+walking distance from station to loc B
+"""
 #sample usage of get_lat_long function
 m = maps()
-address = '419 7th St NW, Washington, DC'
+address = '1900 F Street NW, Washington, DC'
 lat_long = m.get_lat_long(address)
+m.distance_to_location()
 print("Address: ", address)
 print("Lat: ", lat_long['lat'])
 print("Long: ", lat_long['lng'])
